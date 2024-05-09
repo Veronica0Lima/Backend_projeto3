10fda6a7a795180ab9d82219713dc2b815208465
Nery <pedronas@al.insper.edu.br>
2024-05-09 13:43:41 -0300

    Backend v1 - Users

--- chats/urls.py
+++ chats/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('api/users/', views.api_users),
+    path('api/users/<int:user_id>/', views.api_user),
 ]
--- chats/views.py
+++ chats/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.http import Http404
+from .models import User
+from .serializers import UserSerializer
+import requests
+from django.http import JsonResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Olá mundo! Este é o app Chats do projeto WorkFlow.")
+
+@api_view(['GET', 'POST'])
+def api_users(request):
+    users = User.objects.all()
+
+    if request.method == 'POST':
+        new_user_data = request.data
+        print(new_user_data)
+        user = User.objects.create(nome=new_user_data['nome'])
+        user.save()
+
+    serialized_users = UserSerializer(users, many=True)
+    return Response(serialized_users.data)
+
+@api_view(['GET', 'DELETE'])
+def api_user(request, user_id):
+    try:
+        user = User.objects.get(id=user_id)
+        if request.method == 'DELETE':
+            user.delete()
+            print(f"{user.nome} deleted")
+    except User.DoesNotExist:
+        raise Http404()
+    serialized_user = UserSerializer(user)
+    return Response(serialized_user.data)
