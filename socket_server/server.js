b2002b91faef903966d34445e3edc801a3f04829
antoniolucasma <antoniolucasmda@gmail.com>
2024-05-17 21:04:25 -0300

    Tempo atual do logout no socket.io

--- socket_server/server.js
+++ socket_server/server.js
@@ -9,32 +9,38 @@ const io = new Server(server, {
     cors: { origin: '*' }
 });
 
-let connectedUsers = {};
+let users = {};
 
 io.on('connection', (socket) => {
     const userId = socket.handshake.query.userId;
-    connectedUsers[userId] = true;
+    const currentTime = new Date().toISOString();
+
+    users[userId] = { isConnected: true, lastLogin: currentTime };
 
     console.log('user connected', userId);
-    console.log('Current users logged: ', connectedUsers);
+    console.log('Current users logged: ', users);
 
     // Emit the connectedUsers dict to the newly connected user
-    socket.emit('users-connected', connectedUsers);
+    socket.emit('users-connected', users);
 
     // Broadcast the updated connectedUsers dict to all other connected clients
-    socket.broadcast.emit('users-connected', connectedUsers);
+    socket.broadcast.emit('users-connected', users);
 
     socket.on('message', (message) => {
         io.emit('message', message);
     });
 
     socket.on('disconnect', () => {
-        delete connectedUsers[userId];
-        socket.broadcast.emit('user-disconnected', userId);
+        if (users[userId]) {
+            users[userId].isConnected = false;
+            users[userId].lastLogin = new Date().toISOString();
+        }
+        socket.broadcast.emit('user-disconnected', users);
         console.log('user disconnected', userId);
+        console.log('users', users);
 
         // Broadcast the updated connectedUsers dict to all connected clients
-        io.emit('users-connected', connectedUsers);
+        io.emit('users-connected', users);
     });
 });
 
